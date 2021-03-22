bd393b148b47f6d954f4bce23888b39ec4db5d9a
Marcin J <jerzakm@gmail.com>
2021-03-22 09:27:56 +0100

    pre-physics dom setup. - Added a grid to showcase the features better - More space for 'about me' and 'contact' type content

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,98 @@
 <script>
-	export let name;
-	import Physics from './Physics.svelte'
+  import Physics from "./Physics.svelte";
 </script>
 
-
-
 <main>
-	<info>
-		<h1>
-		Svelte DOM Physics
-		</h1>
-		<h2>
-			Adds awesome interaction in form of real-time physics to your DOM elements.
-		</h2>
-		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum libero consequatur corrupti officiis molestias dolores deleniti est saepe adipisci nostrum!</p>
-		<p>More info about the project can be found <strong>HERE</strong></p>
-	</info>
-	<bulletpoints>
-		k
-	</bulletpoints>
+  <info>
+    <div>
+      <h1>Svelte DOM Physics</h1>
+      <h2>
+        Adds awesome interaction in form of real-time physics to your DOM
+        elements.
+      </h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum libero
+        consequatur corrupti officiis molestias dolores deleniti est saepe
+        adipisci nostrum!
+      </p>
+    </div>
+    <div>
+      <p>More info about the project can be found <a href=".">HERE</a></p>
+      <p>
+        Want to contribute, discuss implementation, performance or anything
+        else?
+      </p>
+      <a href=".">Open an issue</a> or <a href=".">Send me a message</a>
+    </div>
+  </info>
+  <div class="grid">
+    <div class="grid-item" />
+    <div class="grid-item grid-item--width2 " />
+    <div class="grid-item " />
+    <div class="grid-item " />
+    <div class="grid-item grid-item--width3" />
+    <div class="grid-item" />
+    <div class="grid-item" />
+    <div class="grid-item " />
+    <div class="grid-item grid-item--width2 " />
+    <div class="grid-item" />
+    <div class="grid-item " />
+    <div class="grid-item" />
+    <div class="grid-item grid-item--width2 " />
+    <div class="grid-item grid-item--width2" />
+    <div class="grid-item" />
+    <div class="grid-item " />
+    <div class="grid-item" />
+    <div class="grid-item" />
+    <div class="grid-item " />
+    <div class="grid-item " />
+    <div class="grid-item" />
+    <div class="grid-item" />
+    <div class="grid-item" />
+  </div>
 </main>
 
 <!-- <Physics/> -->
-
 <style>
-	main {
-		padding: 1rem;
-		display: grid;
-		grid-template-columns: 2fr 3fr;
-		gap: 2rem;
-	}
+  main {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2rem;
+    min-height: 100vh;
+  }
 
-	info{
-		padding: 5rem 2rem 5rem 2rem;
-	}
+  info {
+    padding: 5rem 2rem 5rem 2rem;
+    display: flex;
+    flex-direction: column;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 960px) {
+    main {
+      /* max-width: none; */
+      grid-template-columns: 2fr 3fr;
+    }
+  }
+
+  .grid {
+    background: #eee;
+    width: 100%;
+    padding: 5rem 0 5rem 0;
+    gap: 10rem;
+  }
+  .grid-item {
+    width: 160px;
+    height: 90px;
+    float: left;
+    background: #ff3e00;
+    border-radius: 5px;
+    margin: 1rem;
+  }
+
+  .grid-item--width2 {
+    width: 320px;
+  }
+  .grid-item--width3 {
+    width: 440px;
+  }
 </style>
