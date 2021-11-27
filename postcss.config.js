014ca4e232c7b8523e3bf3a2abd7edd1665d548a
Marcin Jerzak <jerzakm@gmail.com>
2021-11-27 16:10:31 +0100

    tailwind added

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ["./public/index.html", "./src/**/*.svelte"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
