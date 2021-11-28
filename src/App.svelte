66dd1c4335817ac9c0aae3b0c091b012adafaa26
Marcin Jerzak <jerzakm@gmail.com>
2021-11-28 12:42:58 +0100

    page styling with daisy ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-  export let name: string;
 </script>
 
-<main>
-  <h1 class="text-red-500">Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+<main class="hero min-h-screen bg-base-200">
+  <div class="text-center grid grid-cols-3">
+    <div>
+      <h1 class="mb-5 text-5xl font-bold">
+        DOM Physics with Svelte and Rapier.rs
+      </h1>
+      <p>
+        <span>Simple proof of concept for a fun user interaction.</span>
+        <span
+          >We pretend that elements of the webpage are rectangles in a 2D
+          physics world.</span
+        >
+      </p>
+      <p><span>Press the button and let the magic happen.</span></p>
+
+      <button class="btn btn-primary mt-5">Let's get physical</button>
+
+      <ul>
+        <b>Powered by:</b>
+        <li>Rapier.rs</li>
+        <li>Svelte</li>
+        <li>Typescript</li>
+        <li>Webworkers</li>
+      </ul>
+    </div>
+  </div>
 </main>
